7c69bed803246cb7804c0fe649aad79c083318ee
7MADA17 <ah9105499@gmail.com>
2025-01-16 19:59:26 +0000

    accept

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,9 +9,9 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              MONCY
+              Ahmed
               <br />
-              <span>YOHANNAN</span>
+              <span>Rami</span>
             </h1>
           </div>
           <div className="landing-info">
